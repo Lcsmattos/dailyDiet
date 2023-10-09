898ab447cada046f4f07847a34dbc029117cb086
Lucas Mattos <lcsmattos7@gmail.com>
2023-10-09 14:28:07 -0300

    criação do theme da aplicação

---
+++ src/theme/index.ts
@@ -0,0 +1,36 @@
+export default {
+	COLORS: {
+		WHITE: "#FFFFFF",
+		GRAY: {
+			HUE_100: "#FAFAFA",
+			HUE_200: "#EFF0F0",
+			HUE_300: "#DDDEDF",
+			HUE_400: "#B9BBBC",
+			HUE_500: "#5C6265",
+			HUE_600: "#333638",
+			HUE_700: "#1B1D1E",
+		},
+		RED: {
+			LIGHT: "#F4E6E7",
+			MID: "#F3BABD",
+			DARK: "#BF3B44",
+		},
+		GREEN: {
+			LIGHT: "#E5F0DB",
+			MID: "#CBE4B4",
+			DARK: "#639339",
+		},
+	},
+	FONT_FAMILY: {
+		REGULAR: "NunitoSans_400Regular",
+		BOLD: "NunitoSans_700Bold",
+	},
+	FONT_SIZE: {
+		XS: 12,
+		SM: 14,
+		MD: 16,
+		LG: 18,
+		XL: 24,
+		XXL: 32,
+	},
+};
