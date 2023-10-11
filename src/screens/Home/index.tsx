63f6ced6b9b72da171aad8f05befb5d8f8ee796b
Lucas Mattos <lcsmattos7@gmail.com>
2023-10-10 23:26:15 -0300

    Criação da página Home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+import { Input } from "@components/Input";
+
+export function Home() {
+	return (
+		<Container>
+			<Input label="Teste" placeholder="Insira aqui " />
+		</Container>
+	);
+}
